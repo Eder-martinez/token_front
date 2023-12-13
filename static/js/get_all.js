1023456837a613b484b14665df857cf10ce7bd9b
Eder-martinez <122801090+Eder-martinez@users.noreply.github.com>
2023-12-12 19:26:01 -0600

    Update get_all.js

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,62 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    getAll();
-});
-
 function getAll() {
-    fetch('http://localhost:8000/contactos')
-        .then(response => response.json())
-        .then(data => {
-            const tbody_contactos = document.getElementById("tbody_contactos");
-            tbody_contactos.innerHTML = "";
+    const URL = "https://back-contactos.onrender.com/contactos";  // Ruta correcta del servidor FastAPI
+    var request = new XMLHttpRequest();
+    request.open('GET', URL);
+    request.send();
 
-            data.forEach(contacto => {
-                const tr = document.createElement("tr");
-                tr.innerHTML = `
-                    <td>${contacto.email}</td>
-                    <td>${contacto.nombre}</td>
-                    <td>${contacto.telefono}</td>
-                    <td><button onclick="eliminarContacto('${contacto.email}')">Eliminar</button></td>
-                `;
-                tbody_contactos.appendChild(tr);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
+    request.onload = (e) => {
+        const response = request.responseText;
+        const json = JSON.parse(response);
+        console.log("response: " + response);
+        console.log("json: " + json);
+        console.log("status_code: " + request.status);
 
-function insertarContacto() {
-    const email = document.getElementById("email").value;
-    const nombre = document.getElementById("nombre").value;
-    const telefono = document.getElementById("telefono").value;
+        console.log("Email: " + json[0]["email"]);
+        console.log("Nombre: " + json[0]["nombre"]);
+        console.log("Telefono: " + json[0]["telefono"]);
 
-    const nuevoContacto = {
-        email: email,
-        nombre: nombre,
-        telefono: telefono
+        const tbody_contactos = document.getElementById("tbody_contactos");
+        for (var i = 0; i < Object.keys(json).length; i++) {
+            var tr = document.createElement("tr");
+            var td_email = document.createElement("td");
+            var td_nombre = document.createElement("td");
+            var td_telefono = document.createElement("td");
+
+            td_email.innerHTML = json[i]["email"];
+            td_nombre.innerHTML = json[i]["nombre"];
+            td_telefono.innerHTML = json[i]["telefono"];
+
+            console.log("Email: " + json[i]["email"]);
+
+            tr.appendChild(td_email);
+            tr.appendChild(td_nombre);
+            tr.appendChild(td_telefono);
+            tbody_contactos.appendChild(tr);
+        }
     };
-
-    fetch('http://localhost:8000/contactos', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(nuevoContacto),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Contacto insertado:', data);
-            getAll();
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-function eliminarContacto(email) {
-    fetch(`http://localhost:8000/contactos/${email}`, {
-        method: 'DELETE',
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Contacto eliminado:', data);
-            getAll();
-        })
-        .catch(error => console.error('Error:', error));
 }
